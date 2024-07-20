a014878f1148de6cae5447dea5c6f6e0de5932eb
stephy <stephshiro35@gmail.com>
2024-07-20 21:33:48 +0300

    add navbar and about

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { FaOpencart } from "react-icons/fa";
+import { SlUser } from "react-icons/sl";
+import { Link } from "react-router-dom";
+
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="sticky top-0 bg-amber-50 shadow-md z-50">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-24">
+          <div className="flex items-center">
+            <div className="flex-shrink-0 flex items-center">
+              <img
+                className="h-18 w-32 mr-2"
+                src="src/assets/sweetrepeatthrifts-high-resolution-logo-black-transparent.png"
+                alt="Sweet Repeats Thrift Logo"
+              />
+              <h1 className="text-xl font-bold text-black"></h1>
+            </div>
+            <div className="hidden md:block">
+              <div className="ml-80 flex items-baseline space-x-4">
+                <a
+                  href="#"
+                  className="text-black hover:text-slate-600 px-3 py-2"
+                >
+                  Home
+                </a>
+                <a
+                  href="#"
+                  className="text-black hover:text-slate-600 px-3 py-2"
+                >
+                  About
+                </a>
+                <a
+                  href="#"
+                  className="text-black hover:text-slate-600 px-3 py-2"
+                >
+                  Shop
+                </a>
+
+                <a
+                  href="#"
+                  className="text-black hover:text-slate-600 px-3 py-2"
+                >
+                  Contact
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="hidden md:block -m-10">
+            <button className="text-black p-1 m-3">
+              <FaOpencart className="h-6 w-6" />
+            </button>
+            <button className="text-black p-1 m-3">
+              <SlUser className="h-6 w-6" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/About.tsx
@@ -0,0 +1,185 @@
+const About= () => {
+    const cardStyle =
+      "rounded-lg p-6 bg-white shadow-lg ";
+  
+    return (
+      <div className="w-screen">
+        <section
+          className="bg-cover bg-center relative w-screen  mx-auto pt-15"
+          style={{
+            backgroundImage: `url('src/assets/About2.jpeg')`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundAttachment: "fixed",
+            height: "300px",
+            width: "100%",
+          }}>
+          <div
+            className="flex bg-black bg-opacity-50 text-white backdrop-filter backdrop-blur-sm mx-auto px-0"
+            style={{
+              backdropFilter: "blur(5px)",
+              height: "300px",
+              width: "100%",
+              fontWeight: "bold",
+              display: "flex",
+              alignItems: "center",
+            }}>
+            <h2
+              className="text-6xl  mx-auto px-6 py-8"
+              >
+              About
+            </h2>
+          </div>
+        </section>
+  
+        <section className="bg-amber-50 w-screen text-black py-20 text-center justify-center">
+          <div className="container mx-auto">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+             
+              <div
+                className={cardStyle}
+                style={{ position: "relative" }}>
+                <h2 className="text-4xl font-bold mb-4 text-[#002147]">
+                  Our Vision
+                </h2>
+                <p className="text-lg">
+                  Our vision is to be the leading cleaning service provider known
+                  for our innovative solutions and customer-centric approach. We
+                  aim to set the standard for cleanliness and hygiene in the
+                  industry.
+                </p>
+              </div>
+              <div
+                className={cardStyle}
+                style={{ position: "relative" }}>
+                <h2 className="text-4xl font-bold mb-4 text-[#002147]">
+                  Our Values
+                </h2>
+                <ul className="text-lg list-disc pl-5">
+                  <li className="mb-2">
+                    Integrity: We operate with honesty and integrity in all that
+                    we do.
+                  </li>
+                  <li className="mb-2">
+                    Excellence: We are committed to delivering top-quality
+                    services.
+                  </li>
+                  <li className="mb-2">
+                    Sustainability: We prioritize eco-friendly practices in our
+                    operations.
+                  </li>
+                  <li className="mb-2">
+                    Customer Satisfaction: We aim to exceed our customers'
+                    expectations.
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+  
+          <div className="p-8 pt-24 flex space-x-8 text-[#002147]">
+            <div className="img flex-shrink-0 w-2/5 pt-8 ">
+              <img
+                src="/src/assets/CCCLean.jpg"
+                alt="Demo pic"
+                className="rounded-xl shadow-2xl w-full h-full object-cover"
+              />
+            </div>
+            <div className="content flex-grow pt-16 text-left">
+              <h2 className="text-l font-bold mb-4 text-left text-[#53CCDC]">
+                About Us
+              </h2>
+              <p className="text-3xl mb-4 text-left font-bold ">
+                Experiences on cleaning area with successful project ongoing.
+              </p>
+              <p className="mb-8 text-left">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam.
+              </p>
+  
+              <ul className="list-none mb-4 grid grid-cols-2 gap-x-4 gap-y-2 font-bold">
+                <li className="flex items-center mb-2">
+                  <svg
+                    className="w-5 h-5 text-green-500 mr-2"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M5 13l4 4L19 7"
+                    />
+                  </svg>
+                  Experienced Team
+                </li>
+                <li className="flex items-center mb-2">
+                  <svg
+                    className="w-5 h-5 text-green-500 mr-2"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M5 13l4 4L19 7"
+                    />
+                  </svg>
+                  Weekly or fortnightly
+                </li>
+                <li className="flex items-center mb-2">
+                  <svg
+                    className="w-5 h-5 text-green-500 mr-2"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M5 13l4 4L19 7"
+                    />
+                  </svg>
+                  All in all service
+                </li>
+                <li className="flex items-center mb-2">
+                  <svg
+                    className="w-5 h-5 text-green-500 mr-2"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M5 13l4 4L19 7"
+                    />
+                  </svg>
+                  Book, manage & pay
+                </li>
+              </ul>
+  
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore
+              </p>
+  
+              <div className="border-t-4 border-black my-4"></div>
+  
+              <p className="mt-8 font-semibold">Rosalina D. William Founder</p>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  };
+  
+  export default About;
+  
